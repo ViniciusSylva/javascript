9cf5030464d2d679615b0e2c0b8cae0e5adcce8b
Vinicius Silva <vinisilva7494@gmail.com>
2025-05-07 10:18:02 -0300

    js

--- exercicios/aula12ex/ex015/script.js
+++ exercicios/aula12ex/ex015/script.js
@@ -8,5 +8,34 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade <= 10) {
+                img.setAttribute('src', 'menininho.png')
+            } else if (idade > 10 && idade < 21) {
+                img.setAttribute('src', 'moço.png')
+            } else if (idade > 21 && idade < 50) {
+                img.setAttribute('src', 'adulto.png')
+            } else {
+                img.setAttribute('src', 'idoso.png')
+            }
+        } else {
+            genero = 'Mulher'
+            if (idade >= 0 && idade <= 10) {
+                img.setAttribute('src', 'menininha.png')
+            } else if (idade > 10 && idade < 21) {
+                img.setAttribute('src', 'moça.png')
+            } else if (idade > 21 && idade < 50) {
+                img.setAttribute('src', 'adulta.png')
+            } else {
+                img.setAttribute('src', 'idosa.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
